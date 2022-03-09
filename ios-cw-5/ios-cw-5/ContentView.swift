f87cdb2433bfaa8758b71d0dca658464d6eb1a33
maryam matiri <64448234+Lalique-art@users.noreply.github.com>
2022-03-09 20:44:34 +0300

    done

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,10 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack {
+            HStack{
+                Image("school")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .padding()
+                    .frame(width: 200.0)
+                Text("school life")
+                    .font(.title2)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+           
+            HStack{
+                Image("at school")
+                    .resizable()
+                    .frame(width:130, height:130)
+                Image("mood")
+                    .resizable()
+                    .frame(width:130, height:130)
+                Image("school morning")
+                    .resizable()
+                    .frame(width:130, height:130)
+                
+            }
+            
+            HStack{
+                Image("school")
+                    .resizable()
+                    .frame(width:130, height:130)
+                Image("morning person")
+                    .resizable()
+                    .frame(width:130, height:130)
+                Image("such a mood")
+                    .resizable()
+                    .frame(width:130, height:130)
+                
+            }
+            Spacer()
+                
+            
+                
+                
+                
+            }
+    
+        }
+    
+        
+            }
+        
+    
+
+
+    
+           
+       
+    
+
+
+    
+
+    
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
